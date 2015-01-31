e7d8ca9b70d190314b861ad1f299d3db0a2dcdcd
sikang <lskwdlskwd@gmail.com>
2015-01-31 17:27:09 -0500

    add new trajectory generator

---
+++ mrsl_quadrotor_launch/scripts/line_tracker/send_waypoints.sh
@@ -0,0 +1,5 @@
+rostopic pub -1 controllers_manager/line_tracker_waypoints/waypoints nav_msgs/Path  "poses: [
+{pose: {position: {x: 0.5, y: 0.0, z: 1.5}, orientation: {w: 1.0, x: 0.0, y: 0.0, z: 0.0}}}, 
+{pose: {position: {x: 0.5, y: 12.0, z: 1.5}, orientation: {w: 1.0, x: 0.0, y: 0.0, z: 0.0}}},
+{pose: {position: {x: 3.0, y: 13.0, z: 1.5}, orientation: {w: 0.86602540378, x: 0.0, y: 0.0, z: 0.5}}}
+  ]"
--- mrsl_quadrotor_launch/scripts/send_waypoints.sh
+++
@@ -1,5 +0,0 @@
-rostopic pub -1 controllers_manager/line_tracker_waypoints/waypoints nav_msgs/Path  "poses: [
-{pose: {position: {x: 0.0, y: 0.0, z: 1.0}, orientation: {w: 1.0, x: 0.0, y: 0.0, z: 0.0}}}, 
-{pose: {position: {x: 2.0, y: 0.0, z: 1.0}, orientation: {w: 1.0, x: 0.0, y: 0.0, z: 0.0}}},
-{pose: {position: {x: 2.0, y: 1.0, z: 1.0}, orientation: {w: 0.86602540378, x: 0.0, y: 0.0, z: 0.5}}}
-  ]"
