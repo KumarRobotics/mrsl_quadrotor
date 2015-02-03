0a22cff1f038736374c4567044ae2b9b1fb88fa1
sikang <lskwdlskwd@gmail.com>
2015-02-03 14:07:03 -0500

    add go_to_chairs.sh

---
+++ mrsl_quadrotor_launch/scripts/trajectory_generator/go_to_chairs.sh
@@ -0,0 +1,7 @@
+rostopic pub -1 waypoints nav_msgs/Path  "poses: [
+{pose: {position: {x: 0.0, y: 0.0, z: 1.5}, orientation: {w: 0.923879535, x: 0.0, y: 0.0, z: 0.3826834}}}, 
+{pose: {position: {x: 0.5, y: 0.0, z: 1.5}, orientation: {w: 0.923879535, x: 0.0, y: 0.0, z: 0.3826834}}},
+{pose: {position: {x: 0.5, y: 13, z: 1.5}, orientation: {w: 0.923879535, x: 0.0, y: 0.0, z: 0.3826834}}},
+{pose: {position: {x: 2.0, y: 13, z: 1.5}, orientation: {w: 0.923879535, x: 0.0, y: 0.0, z: 0.3826834}}},
+{pose: {position: {x: 2.0, y: 13, z: 1.0}, orientation: {w: 1.0, x: 0.0, y: 0.0, z: 0.0}}}
+]"
---
+++ mrsl_quadrotor_launch/scripts/trajectory_generator/go_to_pose.sh
@@ -0,0 +1,4 @@
+rostopic pub -1 waypoints nav_msgs/Path  "poses: [
+{pose: {position: {x: 2.0, y: 11, z: 1.5}, orientation: {w: 1.0, x: 0.0, y: 0.0, z: 0.0}}},
+{pose: {position: {x: 2.0, y: 11, z: 0.5}, orientation: {w: 1.0, x: 0.0, y: 0.0, z: 0.0}}}
+]"
