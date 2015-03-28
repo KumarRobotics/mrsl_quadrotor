184854d9b53842bb95c2b5b8ef89341881e87e4f
sikang <lskwdlskwd@gmail.com>
2015-03-27 23:25:10 -0400

    stable version 0.1

--- mrsl_quadrotor_launch/scripts/trajectory_generator/send_waypoints.sh
+++ mrsl_quadrotor_launch/scripts/trajectory_generator/send_waypoints.sh
@@ -1,5 +1,4 @@
 rostopic pub -1 juliett/waypoints nav_msgs/Path  "poses: [
-{pose: {position: {x: 0.0, y: 0.0, z: 2.0}, orientation: {w: 0.923879535, x: 0.0, y: 0.0, z: 0.3826834}}}, 
-{pose: {position: {x: 2.0, y: -1.0, z: 2.0}, orientation: {w: 0.923879535, x: 0.0, y: 0.0, z: 0.3826834}}}, 
+{pose: {position: {x: 0.0, y: -1.0, z: 2.0}, orientation: {w: 0.923879535, x: 0.0, y: 0.0, z: 0.3826834}}}, 
 {pose: {position: {x: 2.5, y: -1.0, z: 2.0}, orientation: {w: 1.0, x: 0.0, y: 0.0, z: 0.0}}}
 ]"
--- mrsl_quadrotor_launch/scripts/trajectory_generator/test_juliett.sh
+++ mrsl_quadrotor_launch/scripts/trajectory_generator/test_juliett.sh
@@ -1,9 +1,10 @@
-rostopic pub -1 juliett/goals nav_msgs/Path  "{
+rostopic pub -1 juliett/waypoints nav_msgs/Path  "{
 header: {seq: 0, stamp: 0, frame_id: map}, poses: [
 {pose: {position: {x: 0.0, y: 0.0, z: 1.5}, orientation: {w: 0.923879535, x: 0.0, y: 0.0, z: 0.3826834}}}, 
 {pose: {position: {x: 2.0, y: -3.5, z: 1.5}, orientation: {w: 0.923879535, x: 0.0, y: 0.0, z: 0.3826834}}},
 {pose: {position: {x: 8.2, y: -3.5, z: 1.5}, orientation: {w: 0.923879535, x: 0.0, y: 0.0, z: 0.3826834}}},
 {pose: {position: {x: 8.2, y: 11.2, z: 1.5}, orientation: {w: 0.923879535, x: 0.0, y: 0.0, z: 0.3826834}}},
 {pose: {position: {x: -1, y: 11.2, z: 1.5}, orientation: {w: 0.923879535, x: 0.0, y: 0.0, z: 0.3826834}}},
-{pose: {position: {x: -1, y: 0, z: 1.5}, orientation: {w: 0.923879535, x: 0.0, y: 0.0, z: 0.3826834}}}
+{pose: {position: {x: -1, y: 0, z: 1.5}, orientation: {w: 0.923879535, x: 0.0, y: 0.0, z: 0.3826834}}},
+{pose: {position: {x: 0, y: 0, z: 1.5}, orientation: {w: 0.923879535, x: 0.0, y: 0.0, z: 0.3826834}}}
 ]}"
