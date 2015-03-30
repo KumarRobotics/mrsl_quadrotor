e4afe3d290af476c5e0586f3838a6ebdaaa3de97
sikang <lskwdlskwd@gmail.com>
2015-03-30 11:48:18 -0400

    stereo_camera integrated

---
+++ mrsl_quadrotor_launch/scripts/trajectory_generator/test_gazebo.sh
@@ -0,0 +1,5 @@
+rostopic pub -1 romeo/waypoints nav_msgs/Path  "poses: [
+{pose: {position: {x: 0, y: 0, z: 1.5}, orientation: {w: 0.923879535, x: 0.0, y: 0.0, z: 0.3826834}}},
+{pose: {position: {x: 0, y: 0, z: 0.5}, orientation: {w: 0.923879535, x: 0.0, y: 0.0, z: 0.3826834}}},
+{pose: {position: {x: 0, y: 0, z: 1.5}, orientation: {w: 0.923879535, x: 0.0, y: 0.0, z: 0.3826834}}}
+]"
--- mrsl_quadrotor_launch/scripts/trajectory_generator/test_romeo.sh
+++ mrsl_quadrotor_launch/scripts/trajectory_generator/test_romeo.sh
@@ -1,5 +1,5 @@
 rostopic pub -1 romeo/waypoints nav_msgs/Path  "poses: [
-{pose: {position: {x: 0.0, y: 0.0, z: 1.5}, orientation: {w: 0.923879535, x: 0.0, y: 0.0, z: 0.3826834}}}, 
+{pose: {position: {x: 0, y: 0, z: 1.5}, orientation: {w: 0.923879535, x: 0.0, y: 0.0, z: 0.3826834}}},
 {pose: {position: {x: -1.0, y: -3.5, z: 1.5}, orientation: {w: 0.923879535, x: 0.0, y: 0.0, z: 0.3826834}}},
 {pose: {position: {x: 8.2, y: -3.5, z: 1.5}, orientation: {w: 0.923879535, x: 0.0, y: 0.0, z: 0.3826834}}},
 {pose: {position: {x: 8.2, y: -14.2, z: 1.5}, orientation: {w: 0.923879535, x: 0.0, y: 0.0, z: 0.3826834}}},
