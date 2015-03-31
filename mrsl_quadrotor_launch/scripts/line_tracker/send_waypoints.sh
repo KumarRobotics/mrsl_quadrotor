f3f5611bdef1c0397b1e5acd82708b2a9e07d010
sikang <lskwdlskwd@gmail.com>
2015-03-30 22:17:45 -0400

    add house environment for vision

--- mrsl_quadrotor_launch/scripts/line_tracker/send_waypoints.sh
+++ mrsl_quadrotor_launch/scripts/line_tracker/send_waypoints.sh
@@ -1,5 +1,1 @@
-rostopic pub -1 controllers_manager/line_tracker_waypoints/waypoints nav_msgs/Path  "poses: [
-{pose: {position: {x: 0.5, y: 0.0, z: 1.5}, orientation: {w: 1.0, x: 0.0, y: 0.0, z: 0.0}}}, 
-{pose: {position: {x: 0.5, y: 12.0, z: 1.5}, orientation: {w: 1.0, x: 0.0, y: 0.0, z: 0.0}}},
-{pose: {position: {x: 3.0, y: 13.0, z: 1.5}, orientation: {w: 0.86602540378, x: 0.0, y: 0.0, z: 0.5}}}
-  ]"
+rostopic pub -1 /romeo/controllers_manager/line_tracker_distance/goal geometry_msgs/Vector3 "{x: 1.0, y: 2.0, z: 1.0}"
--- mrsl_quadrotor_launch/scripts/line_tracker/start_motor.sh
+++ mrsl_quadrotor_launch/scripts/line_tracker/start_motor.sh
@@ -1,4 +1,4 @@
-rostopic pub -1 /KHexTango/controllers_manager/line_tracker_distance/goal geometry_msgs/Vector3 "{x: 0.0, y: 0.0, z: 0}"
+rostopic pub -1 /romeo/controllers_manager/line_tracker_distance/goal geometry_msgs/Vector3 "{x: 0.0, y: 0.0, z: 0}"
 
-rosservice call /KHexTango/controllers_manager/transition line_tracker/LineTrackerDistance
+rosservice call /romeo/controllers_manager/transition line_tracker/LineTrackerDistance
 
