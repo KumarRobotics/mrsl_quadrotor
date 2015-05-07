7d13ccd0ac4120cd9d4577ac6581eadd2106feae
sikang <lskwdlskwd@gmail.com>
2015-05-06 22:03:48 -0400

    colored skir model

--- mrsl_quadrotor_launch/scripts/trajectory_generator/go_to_pose.sh
+++ mrsl_quadrotor_launch/scripts/trajectory_generator/go_to_pose.sh
@@ -1,5 +1,5 @@
-rostopic pub -1 juliett/goals nav_msgs/Path  "
+rostopic pub -1 juliett/path_w nav_msgs/Path  "
 {header: {seq: 0, stamp: 0, frame_id: map},
 poses: [
-{pose: {position: {x: 10.0, y: 3.5, z: 1.0}, orientation: {w: 1.0, x: 0.0, y: 0.0, z: 0.0}}}
+{pose: {position: {x: 0, y: 0, z: 1.0}, orientation: {w: 1.0, x: 0.0, y: 0.0, z: 0.0}}}
 ]}"
--- mrsl_quadrotor_launch/scripts/trajectory_generator/send_waypoints.sh
+++ mrsl_quadrotor_launch/scripts/trajectory_generator/send_waypoints.sh
@@ -1,4 +1,7 @@
-rostopic pub -1 juliett/waypoints nav_msgs/Path  "poses: [
-{pose: {position: {x: 0.0, y: -1.0, z: 2.0}, orientation: {w: 0.923879535, x: 0.0, y: 0.0, z: 0.3826834}}}, 
-{pose: {position: {x: 2.5, y: -1.0, z: 2.0}, orientation: {w: 1.0, x: 0.0, y: 0.0, z: 0.0}}}
-]"
+rostopic pub -1 juliett/path_w nav_msgs/Path  "
+{header: {seq: 0, stamp: 0, frame_id: map},
+poses: [
+{pose: {position: {x: 0, y: 0, z: 1.0}, orientation: {w: 1.0, x: 0.0, y: 0.0, z: 0.0}}},
+{pose: {position: {x: 0, y: 1.0, z: 0.5}, orientation: {w: 1.0, x: 0.0, y: 0.0, z: 0.0}}},
+{pose: {position: {x: -1, y: 1.0, z: 0.5}, orientation: {w: 1.0, x: 0.0, y: 0.0, z: 0.0}}}
+]}"
