1ca90cf895a06dba38ef486d7b6e0fa8786a55b9
sikang <lskwdlskwd@gmail.com>
2018-01-10 16:57:37 -0500

    update mobile object plugin, change topic name in msg_to_tf

--- mrsl_mobile_object/src/mobile_object.cpp
+++ mrsl_mobile_object/src/mobile_object.cpp
@@ -3,7 +3,9 @@
 #include <gazebo/common/common.hh>
 #include <gazebo/gazebo.hh>
 
-#include <stdio.h>
+#include <ros/ros.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <tf/transform_broadcaster.h>
  
 namespace mrsl_quadrotor_simulator
 {
@@ -25,10 +27,40 @@ namespace mrsl_quadrotor_simulator
     
     void Load(gazebo::physics::ModelPtr _parent, sdf::ElementPtr _sdf)
     {
+      ns_ = "";
+      if (_sdf->HasElement("robotNamespace"))
+        ns_ = _sdf->GetElement("robotNamespace")->Get<std::string>();
+
+      if (!_sdf->HasElement("updateRate"))
+        update_rate_ = 10.0;
+      else
+        update_rate_ = _sdf->GetElement("updateRate")->Get<double>();
+
+
+      if (!_sdf->HasElement("topicName"))
+        topic_name_ = "pose";
+      else
+        topic_name_ = _sdf->Get<std::string>("topicName");
+
+      node_handle_ = new ros::NodeHandle(ns_);
+
+      if (topic_name_ != "") 
+        pub_ = node_handle_->advertise<geometry_msgs::PoseStamped>(topic_name_, 1);
+
       if(_sdf->HasElement("vx"))
         vx_ = _sdf->GetElement("vx")->Get<double>();
+      else
+        vx_ = 0;
       if(_sdf->HasElement("vy"))
         vy_ = _sdf->GetElement("vy")->Get<double>();
+      else
+        vy_ = 0;
+      if (_sdf->HasElement("w"))
+        w_ = _sdf->GetElement("w")->Get<double>();
+      else 
+        w_ = 0;
+
+
 
       link = _parent->GetLink();
 
@@ -55,7 +87,38 @@ namespace mrsl_quadrotor_simulator
         vy_ = -vy_;
 
       link->SetLinearVel(gazebo::math::Vector3(vx_, vy_, 0));
-      link->SetAngularVel(gazebo::math::Vector3(0, 0, 0));
+      link->SetAngularVel(gazebo::math::Vector3(0, 0, w_));
+
+      //if (pub_.getNumSubscribers() > 0 && topic_name_ != "" && update_rate_ > 0)
+      if (topic_name_ != "" && update_rate_ > 0)
+      {
+        static tf::TransformBroadcaster br;
+        static ros::Time prev_time = ros::Time::now();
+
+        if((ros::Time::now() - prev_time).toSec() >= 1./update_rate_) {
+          geometry_msgs::PoseStamped msg;
+          msg.header.stamp = ros::Time::now();
+          msg.header.frame_id = "map";
+          msg.pose.position.x = pose.pos.x;
+          msg.pose.position.y = pose.pos.y;
+          msg.pose.position.z = pose.pos.z;
+          msg.pose.orientation.w = pose.rot.w;
+          msg.pose.orientation.x = pose.rot.x;
+          msg.pose.orientation.y = pose.rot.y;
+          msg.pose.orientation.z = pose.rot.z;
+
+          pub_.publish(msg);
+
+          tf::Transform bt(tf::Quaternion(msg.pose.orientation.x,
+                msg.pose.orientation.y,
+                msg.pose.orientation.z,
+                msg.pose.orientation.w),
+              tf::Vector3(msg.pose.position.x, msg.pose.position.y, msg.pose.position.z));
+          br.sendTransform(tf::StampedTransform(bt, msg.header.stamp, msg.header.frame_id, ns_ + "base_link"));
+
+          prev_time = msg.header.stamp;
+        }
+      }
     }
  
    private:
@@ -66,7 +129,14 @@ namespace mrsl_quadrotor_simulator
     bool initialzed_;
     double lower_bound_;
     double upper_bound_;
-    double vx_, vy_;
+    double vx_, vy_, w_;
+
+    // Ros interface
+    ros::NodeHandle *node_handle_;
+    ros::Publisher pub_;
+    std::string ns_;
+    std::string topic_name_;
+    double update_rate_;
   };
  
   // Register this plugin with the simulator
--- mrsl_quadrotor_utils/src/msg_to_tf.cpp
+++ mrsl_quadrotor_utils/src/msg_to_tf.cpp
@@ -1,20 +1,24 @@
 #include <ros/ros.h>
 #include <tf2_ros/transform_broadcaster.h>
+#include <topic_tools/shape_shifter.h>
 #include <nav_msgs/Odometry.h>
+#include <geometry_msgs/PoseStamped.h>
 
-void odomCallback(const nav_msgs::Odometry::ConstPtr &msg) {
+std::string child_frame_id;
+
+void process(const geometry_msgs::Pose &pose, const std_msgs::Header& header, std::string child_frame_id) {
   static tf2_ros::TransformBroadcaster odom_br;
   geometry_msgs::TransformStamped transformStamped;
 
-  transformStamped.header = msg->header;
-  transformStamped.child_frame_id = msg->child_frame_id;
-  transformStamped.transform.translation.x = msg->pose.pose.position.x;
-  transformStamped.transform.translation.y = msg->pose.pose.position.y;
-  transformStamped.transform.translation.z = msg->pose.pose.position.z;
-  transformStamped.transform.rotation.x = msg->pose.pose.orientation.x;
-  transformStamped.transform.rotation.y = msg->pose.pose.orientation.y;
-  transformStamped.transform.rotation.z = msg->pose.pose.orientation.z;
-  transformStamped.transform.rotation.w = msg->pose.pose.orientation.w;
+  transformStamped.header = header;
+  transformStamped.child_frame_id = child_frame_id;
+  transformStamped.transform.translation.x = pose.position.x;
+  transformStamped.transform.translation.y = pose.position.y;
+  transformStamped.transform.translation.z = pose.position.z;
+  transformStamped.transform.rotation.x = pose.orientation.x;
+  transformStamped.transform.rotation.y = pose.orientation.y;
+  transformStamped.transform.rotation.z = pose.orientation.z;
+  transformStamped.transform.rotation.w = pose.orientation.w;
 
   odom_br.sendTransform(transformStamped);
   ROS_WARN_ONCE("publish tf from [%s] to [%s]",
@@ -22,10 +26,27 @@ void odomCallback(const nav_msgs::Odometry::ConstPtr &msg) {
                 transformStamped.child_frame_id.c_str());
 }
 
+void msgCallback(const topic_tools::ShapeShifter::ConstPtr &msg) {
+  if(msg->getDataType() == "nav_msgs/Odometry") {
+    auto odom = msg->instantiate<nav_msgs::Odometry>();
+    process(odom->pose.pose, odom->header, odom->child_frame_id);
+  }
+  else if(msg->getDataType() == "geometry_msgs/PoseStamped") {
+    auto pose = msg->instantiate<geometry_msgs::PoseStamped>();
+    process(pose->pose, pose->header, child_frame_id);
+  }
+  else 
+    ROS_WARN_ONCE("Unrecognized msg type! [%s]", msg->getDataType().c_str());
+}
+
+
 int main(int argc, char **argv) {
   ros::init(argc, argv, "msg_to_tf");
   ros::NodeHandle nh("~");
-  ros::Subscriber sub = nh.subscribe("odom", 10, odomCallback,
+
+  nh.param("child_frame_id", child_frame_id, std::string("null_frame"));
+
+  ros::Subscriber sub = nh.subscribe("msg", 10, msgCallback,
                                      ros::TransportHints().tcpNoDelay());
 
   ros::spin();
