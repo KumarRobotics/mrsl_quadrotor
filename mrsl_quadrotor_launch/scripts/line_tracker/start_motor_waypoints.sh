9b7cfddc7748b4c5638e2991614184428587db9f
Dinesh Thakur <tdinesh@seas.upenn.edu>
2020-05-21 20:53:11 -0400

    compatible with kr_mav_msgs

--- mrsl_quadrotor_launch/scripts/line_tracker/start_motor_waypoints.sh
+++ mrsl_quadrotor_launch/scripts/line_tracker/start_motor_waypoints.sh
@@ -1,4 +1,4 @@
-rostopic pub -1 controllers_manager/line_tracker_waypoints/goal quadrotor_msgs/FlatOutputs  "{x: 0.0, y: 0.0, z: 0, yaw: 0}"
+rostopic pub -1 controllers_manager/line_tracker_waypoints/goal kr_mav_msgs/FlatOutputs  "{x: 0.0, y: 0.0, z: 0, yaw: 0}"
 
 rosservice call controllers_manager/transition line_tracker/LineTrackerWaypoints
 
